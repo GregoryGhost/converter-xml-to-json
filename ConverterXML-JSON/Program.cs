9932f01d5c5d68846b04a02a71ba64e37b3ce793
Gregory <lobster287@gmail.com>
2018-09-21 17:48:54 +0700

    Added workin version of converter algorithm

--- ConverterXML-JSON/Program.cs
+++ ConverterXML-JSON/Program.cs
@@ -2,6 +2,9 @@
 using System.Xml;
 using Newtonsoft.Json;
 using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace ConverterXML_JSON
 {
@@ -12,71 +15,129 @@ namespace ConverterXML_JSON
 			var xmlStr = "";
 			const int maxArgs = 2;
 			var jsonFilePath = "";
-            
+
 			try
 			{
 				if (args.Length != maxArgs)
-                {
-					throw new ArgumentException("Arguments wrong.");
-                }
+				{
+					throw new ArgumentException(
+                        "Arguments wrong.");
+				}
 
 				var xmlFile = args[0];
 				jsonFilePath = args[1];
 
-				if(!File.Exists(xmlFile))
+				if (!File.Exists(xmlFile))
 				{
-					throw new FileNotFoundException("Input xml file not found.");
+					throw new FileNotFoundException(
+                        "Input xml file not found.");
 				}
 
 				xmlStr = File.ReadAllText(xmlFile);
 
 				var doc = new XmlDocument();
-                doc.LoadXml(xmlStr);
+				doc.LoadXml(xmlStr);
 
-                var json = JsonConvert.SerializeXmlNode(doc);
-				var fJson = json.FormattingJson();
+                var subLayers = doc.SelectNodes("Capability/Layer/Layer");
+
+				var s = subLayers.Cast<XmlNode>()
+						 .Where(n => n.Name.ToLower() == "layer")
+								 .ToList()
+								 .Select(sl =>
+								 {
+									 var subLayer = new SubLayer();
+                                     
+                                     sl.Cast<XmlNode>()
+                                        .ToList()
+                                        .ForEach(sNode =>
+                                        {
+                                            if (sNode.Name.ToLower() == "name")
+                                            {
+                                             subLayer.name = sNode.InnerText;
+                                            }
+                                            if (sNode.Name.ToLower() == "title")
+                                            {
+                                             subLayer.title = sNode.InnerText;
+                                            }
+                                            if(sNode.Name.ToLower()== "attributes")
+                                            {
+                                                 subLayer.attributes = sNode.Cast<XmlNode>()
+                                                     .Select(a =>
+                                                     {
+                                                         return new Attribute
+                                                         {
+                                                            name = a.Attributes["name"]
+                                                                ?.Value ?? "",
+                                                            type = a.Attributes["type"]
+                                                                ?.Value ?? ""
+                                                         };
+                                                     }).ToList();
+                                            }
+                                        });
+                                            
+                                    return subLayer;
+								 }).ToList();
+
+				var layer = new Layer
+                {
+                    name = doc.SelectSingleNode("Capability/Layer/Name")
+                        ?.InnerText ?? "",
+				    title = doc.SelectSingleNode("Capability/Layer/Title")
+                        ?.InnerText ?? "",
+                    sublayers = s
+                };
+
+                var fJson = JsonConvert.SerializeObject(layer)
+                    .FormattingJson();
+
 				Console.WriteLine("Given JSON:");
-                Console.WriteLine(fJson);
-                Console.WriteLine();
+				Console.WriteLine(fJson);
+				Console.WriteLine();
 
 				File.WriteAllText(jsonFilePath, fJson);
 				var msgConvertComplete = String.Format(
-                    "The conversion of the input" +
-                    " file was successfully completed.\n" +
-                    " The data is written to " +
-                    "a file along the path {0}",
-                    jsonFilePath);
-                Console.WriteLine(msgConvertComplete);
+					"The conversion of the input" +
+					" file was successfully completed.\n" +
+					"The data is written to " +
+					"a file along the path {0}",
+					jsonFilePath);
+				Console.WriteLine(msgConvertComplete);
 			}
-			catch(Exception e)
+			catch (Exception e)
 			{
-				Console.WriteLine(e.Message);
+				Console.WriteLine("Error:{0}", e.Message);
+                Console.WriteLine("Stack trace:{0}", e.StackTrace);
 			}
-                     
-			//var alex = new Test();
-			//alex.Age = 12;
-			//alex.Name = "Alex";
-			//alex.SurName = "Nim";
-
-			//var jAlex = JsonConvert.SerializeObject(alex);
-			//Console.WriteLine(jAlex.FormattingJson());
 		}
 	}
 
-    class Test
+	public class Layer
 	{
-		public string Name;
-		public string SurName;
-		public int Age;
+		public string name = "";
+		public string title = "";
+		public List<SubLayer> sublayers;
 	}
 
-    public static class StrExt
+	public class SubLayer
+	{
+		public string name = "";
+		public string title = "";
+		public List<Attribute> attributes;
+	}
+
+	public class Attribute
+	{
+		public string name = "";
+		public string type = "";
+	}
+
+	public static class StrExt
 	{
 		public static string FormattingJson(this string json)
-        {
-            dynamic parsedJson = JsonConvert.DeserializeObject(json);
-            return JsonConvert.SerializeObject(parsedJson,
-                                               Newtonsoft.Json.Formatting.Indented);
-        }
+		{
+			dynamic parsedJson = JsonConvert.DeserializeObject(json);
+			return JsonConvert.SerializeObject(parsedJson,
+											   Newtonsoft.Json.Formatting.Indented);
+		}
 	}
 }
