001a6da93c0f181ed03f75f80ede6191be8a60de
Gregory <lobster287@gmail.com>
2018-09-23 03:31:33 +0700

    Added xml comments for helpers

--- ConverterXML-JSON/Helpers.cs
+++ ConverterXML-JSON/Helpers.cs
@@ -3,20 +3,47 @@ using System.Xml;
 
 namespace ConversionTools
 {
+    /// <summary>
+    /// Фабрика с объектами для
+    ///     конвертации данных.
+    /// </summary>
     public interface IConversionFactory<T>
     {
+        /// <summary>
+        /// Создать парсер.
+        /// </summary>
+        /// <returns>Возвращает парсер.</returns>
         IParser<T> CreateParser();
+        
+        /// <summary>
+        /// Создает "Преобразователь".
+        /// </summary>
+        /// <returns>Возвращает преобразователь.</returns>
         IMapper<T> CreateMapper();
     }
-    
 
+    
+    /// <summary>
+    /// Фабрика с объектами для
+    ///     конвертации из XML в JSON.
+    /// </summary>
     public class ConversionFactory: IConversionFactory<XmlNode>
     {
+        /// <summary>
+        /// Создать парсер для XML документа.
+        /// </summary>
+        /// <returns>Возвращает парсер
+        ///     для XML документа.</returns>
         public IParser<XmlNode> CreateParser()
         {
             return new ParserXml();
         }
 
+        /// <summary>
+        /// Создать преобразователь для XML документа.
+        /// </summary>
+        /// <returns>Возвращает преобразователь
+        ///     для XML документа.</returns>
         public IMapper<XmlNode> CreateMapper()
         {
             return new MapperXmlToLayers();
@@ -24,10 +51,20 @@ namespace ConversionTools
     }
 
 
+    /// <summary>
+    /// Стеганографист.
+    /// </summary>
     public static class Logger
     {
-        public static void WriteSuccessConversion(string json,
-            string pathToJsonFile)
+        /// <summary>
+        /// Записать в лог успешность конвертации.
+        /// </summary>
+        /// <param name="json">Данные в формате
+        ///     JSON.</param>
+        /// <param name="pathToJsonFile">Путь до
+        ///     JSON файла.<param>
+        public static void WriteSuccessConversion(
+            string json, string pathToJsonFile)
         {
             Console.WriteLine("Given JSON:");
             Console.WriteLine(json);
