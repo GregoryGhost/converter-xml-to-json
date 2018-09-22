5da872f75d1dd4c7b299c6fa101ae5773b4ca6b9
Gregory <lobster287@gmail.com>
2018-09-23 02:29:20 +0700

    Added xml comments for Parser

--- ConverterXML-JSON/Parser.cs
+++ ConverterXML-JSON/Parser.cs
@@ -6,31 +6,66 @@ using System.Collections.Generic;
 
 namespace ConversionTools
 {
+    /// <summary>
+    /// Парсер некой структуры данных.
+    /// </summary>
     public interface IParser<T>
     {
+        /// <summary>
+        /// Найти ключевые узлы в структуре данных.
+        /// </summary>
+        /// <returns>Возвращает ключевые узлы.</returns>
         List<T> FindKeyNodes();
-        
+
+        /// <summary>
+        /// Парсер некой структуры данных.
+        /// </summary>
         List<T> KeyNodes { get; }
 
+        /// <summary>
+        /// Парсер некой структуры данных.
+        /// </summary>
         string InputData { get; set; }
     }
-    
 
+    
+    /// <summary>
+    /// Парсер XML документа определенной структуры.
+    /// </summary>
     public class ParserXml: IParser<XmlNode>
     {
+        /// <summary>
+        /// Ключевые теги в XML документе.
+        /// </summary>
         private List<XmlNode> _keyNodes;
 
+        /// <summary>
+        /// Входные данные XML документа.
+        /// </summary>
         private string _inputXml;
 
+        /// <summary>
+        /// XML документ.
+        /// </summary>
         private XmlDocument _doc;
 
+        /// <summary>
+        /// Корневой тег.
+        /// </summary>
         private const string _tagRoot = "Capability/Layer/*";
         
+        /// <summary>
+        /// Инициализация парсера XML документа определенной структуры.
+        /// </summary>
         public ParserXml()
         {
             _doc = new XmlDocument();
         }
         
+        /// <summary>
+        /// Найти ключевые теги в XML документе.
+        /// </summary>
+        /// <returns>Возвращает ключевые теги.</returns>
         public List<XmlNode> FindKeyNodes()
         {
             _doc.LoadXml(_inputXml);
@@ -39,6 +74,9 @@ namespace ConversionTools
             return _keyNodes;
         }
 
+        /// <summary>
+        /// Ключевые теги в XML документе.
+        /// </summary>
         public List<XmlNode> KeyNodes
         {
             get
@@ -51,7 +89,10 @@ namespace ConversionTools
                 this._keyNodes = value;
             }
         }
-
+        
+        /// <summary>
+        /// Входные данные XML документа.
+        /// </summary>
         public string InputData
         {
             get
