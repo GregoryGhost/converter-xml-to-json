d6d018347e0d622d8d8fbf7dbb7caf4d821a1c32
Gregory <lobster287@gmail.com>
2018-09-23 03:23:02 +0700

    Added xml comments for Converter

--- ConverterXML-JSON/Converter.cs
+++ ConverterXML-JSON/Converter.cs
@@ -3,12 +3,31 @@ using System.Xml;
 
 namespace ConversionTools
 {
+    /// <summary>
+    /// Базовый класс для "Преобразователя"
+    ///     из одного формата данных в другой.
+    /// </summary>
     public abstract class ConverterBase<T>
     {
+        /// <summary>
+        /// Парсер некой структуры данных.
+        /// </summary>
         protected IParser<T> _parser;
+
+        /// <summary>
+        /// Преобразователь узлов некой структуры
+        ///     данных.
+        /// </summary>
         protected IMapper<T> _mapper;
+
+        /// <summary>
+        /// Преобразованные данные.
+        /// </summary>
         protected string _outputData;
-        
+
+        /// <summary>
+        /// Инициализация "Преобразователя" данных.
+        /// </summary>
         public ConverterBase(string inputData,
             IConversionFactory<T> cf)
         {
@@ -16,16 +35,39 @@ namespace ConversionTools
             this._parser.InputData = inputData;
             this._mapper = cf.CreateMapper();
         }
+        
+        /// <summary>
+        /// Преобразовать данные
+        ///     из одного формата в другой.
+        /// </summary>
         public abstract string Process();
+
+        /// <summary>
+        /// Преобразованные данные.
+        /// </summary>
         public abstract string OutputData { get; }
     }
 
 
+    /// <summary>
+    /// Преобразователь из XML в JSON.
+    /// </summary>
     public class ConverterXmlToJson: ConverterBase<XmlNode>
     {
+        /// <summary>
+        /// Преобразователь из XML в JSON.
+        /// </summary>
+        /// <param name="xmlData">XML документ.</param>
+        /// <param name="cf">Фабрика с класса
+        ///     для конвертации данных.</param>
         public ConverterXmlToJson(string xmlData,
             ConversionFactory cf): base(xmlData, cf) { }
 
+        /// <summary>
+        /// Преобразовать из XML в JSON.
+        /// </summary>
+        /// <returns>Возвращает данные в
+        ///     формате JSON.</returns>
         public override string Process()
         {
             _outputData = _mapper.MapNodes(
@@ -33,7 +75,10 @@ namespace ConversionTools
                 
             return _outputData;
         }
-
+        
+        /// <summary>
+        /// Преобразованные данные в формате JSON.
+        /// </summary>
         public override string OutputData => this._outputData;
     }
 }
